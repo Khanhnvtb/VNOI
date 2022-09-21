e53d5d423c82fb47162b632119f5ff41066b4365
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-09-21 16:50:31 +0700

    Add files via upload

---
+++ Bedao Testing Contest 01 - FRAC.cpp
@@ -0,0 +1,72 @@
+//
+// Created by khanhnvtb on 9/21/2022.
+//
+
+//https://oj.vnoi.info/problem/bedao_t1_frac
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct data {
+    int x, y;
+};
+
+vector<int> v;
+
+void createPrimeNumber() {
+    vector<int> prime(10000005, 0);
+    for (int i = 2; i * i <= 10000000; i++)
+        if (prime[i] == 0) {
+            for (int j = 2 * i; j <= 10000000; j += i)
+                prime[j] = 1;
+        }
+    for (int i = 2; i <= 10000000; i++)
+        if (prime[i] == 0) v.push_back(i);
+}
+
+long long power(long long x, int y) {
+    if (y == 0) return 1;
+    if (y == 1) return x;
+    long long tmp = power(x, y / 2);
+    if (y % 2 == 1) return tmp * tmp * x;
+    return tmp * tmp;
+}
+
+int main() {
+    createPrimeNumber();
+    int n;
+    cin >> n;
+    int a[n];
+    bool check = true;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) {
+        if (a[i] <= 0) {
+            check = false;
+            break;
+        }
+    }
+    if (check) {
+        unordered_map<int, data> m;
+        for (int i = 0; i < n; i++) {
+            int cnt, j = 0;
+            while (a[i] > 1) {
+                cnt = 0;
+                if (a[i] % v[j] == 0) {
+                    while (a[i] % v[j] == 0) {
+                        cnt++;
+                        a[i] /= v[j];
+                    }
+                    m[v[j]].x += cnt;
+                    if (m[v[j]].y < cnt) m[v[j]].y = cnt;
+                }
+                j++;
+            }
+        }
+        long long ans = 1;
+        for (auto i: m)
+            ans *= power((long long) i.first, i.second.x - i.second.y);
+        cout << ans;
+    } else cout << "impossible";
+    return 0;
+}
