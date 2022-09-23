acca738d1bca8701f496bfc945421ef4f8012ae3
Khanhnvtb <91838527+Khanhnvtb@users.noreply.github.com>
2022-09-23 11:34:37 +0700

    Update Bedao Testing Contest 01 - FRAC.cpp

--- Bedao Testing Contest 01 - FRAC.cpp
+++ Bedao Testing Contest 01 - FRAC.cpp
@@ -15,20 +15,20 @@ struct data {
 vector<int> v;
 
 void createPrimeNumber() {
-    vector<int> prime(10000005, 0);
-    for (int i = 2; i * i <= 10000000; i++)
+    vector<int> prime(15000005, 0);
+    for (int i = 2; i * i <= 15000000; i++)
         if (prime[i] == 0) {
-            for (int j = 2 * i; j <= 10000000; j += i)
+            for (int j = 2 * i; j <= 15000000; j += i)
                 prime[j] = 1;
         }
-    for (int i = 2; i <= 10000000; i++)
+    for (int i = 2; i <= 15000000; i++)
         if (prime[i] == 0) v.push_back(i);
 }
 
-long long power(long long x, int y) {
+unsigned long long power(unsigned long long x, int y) {
     if (y == 0) return 1;
     if (y == 1) return x;
-    long long tmp = power(x, y / 2);
+    unsigned long long tmp = power(x, y / 2);
     if (y % 2 == 1) return tmp * tmp * x;
     return tmp * tmp;
 }
@@ -37,15 +37,17 @@ int main() {
     createPrimeNumber();
     int n;
     cin >> n;
-    int a[n];
+    int a[n], tmp = 0;
     bool check = true;
     for (int i = 0; i < n; i++) cin >> a[i];
     for (int i = 0; i < n; i++) {
-        if (a[i] <= 0) {
+        if (a[i] == 0) {
             check = false;
             break;
         }
+        if (a[i] < 0) tmp++;
     }
+    if (tmp == n) check = false;
     if (check) {
         unordered_map<int, data> m;
         for (int i = 0; i < n; i++) {
@@ -63,9 +65,10 @@ int main() {
                 j++;
             }
         }
-        long long ans = 1;
-        for (auto i: m)
-            ans *= power((long long) i.first, i.second.x - i.second.y);
+        unsigned long long ans = 1;
+        for (auto i: m) {
+            ans *= power((unsigned long long) i.first, i.second.x - i.second.y);
+        }
         cout << ans;
     } else cout << "impossible";
     return 0;
